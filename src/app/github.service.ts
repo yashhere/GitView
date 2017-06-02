9bb5ff541355586c188a2ceff2145339e5dd220c
Yash Agarwal <yashagarwaljpr@gmail.com>
2017-06-02 20:42:52 +0530

    API FETCH ERRORS, implement userview

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,20 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/forkJoin';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class GithubService {
 
-  private _url = 'https://api.github.com/users/';
+  private _url;
+  private _repos;
+  private _followers;
+  private _following;
+  private _starred_url;
 
   constructor(private _http: Http) {
 
   }
 
-  getUserDetail = function (user) {
-    return this._http.get(this._url + user)
+  getUserDetail = function (userName) {
+    this._url = 'https://api.github.com/users/' + userName;
+    this._repos = this._url + '/repos';
+    this._followers = this._url + '/followers';
+    this._following = this._url + '/following';
+    this._starred_url = this._url + '/starred';
+
+    console.log(this._url, this._repos, this._followers, this._following, this._starred_url);
+
+    const user = this._http.get(this._url)
       .map(res => res.json());
-  }
+
+    const repos = this._http.get(this._repos)
+      .map(res => res.json());
+
+    const followers = this._http.get(this._followers)
+      .map(res => res.json());
+
+    const following = this._http.get(this._following)
+      .map(res => res.json());
+
+    const stars = this._http.get(this._starred_url)
+      .map(res => res.json());
+
+    return Observable
+      .forkJoin(user, repos, followers, following, stars)
+      .map(joined => new Object({
+        userDetail: joined[0],
+        repos: joined[1],
+        followers: joined[2],
+        following: joined[3],
+        stars: joined[4],
+      }));
+  };
 
 }
--- src/app/userview/userview.component.ts
+++ src/app/userview/userview.component.ts
@@ -13,29 +13,64 @@ import 'rxjs/add/operator/switchMap';
 
 export class UserViewComponent implements OnInit {
 
-  userDetail;
-  username;
-  isLoading: Boolean = true;
+  details;
+  // userDetail = {
+  //   'login': 'yash2696',
+  //   'id': 13733968,
+  //   'avatar_url': 'https://avatars1.githubusercontent.com/u/13733968?v=3',
+  //   'gravatar_id': '',
+  //   'url': 'https://api.github.com/users/yash2696',
+  //   'html_url': 'https://github.com/yash2696',
+  //   'followers_url': 'https://api.github.com/users/yash2696/followers',
+  //   'following_url': 'https://api.github.com/users/yash2696/following{/other_user}',
+  //   'gists_url': 'https://api.github.com/users/yash2696/gists{/gist_id}',
+  //   'starred_url': 'https://api.github.com/users/yash2696/starred{/owner}{/repo}',
+  //   'subscriptions_url': 'https://api.github.com/users/yash2696/subscriptions',
+  //   'organizations_url': 'https://api.github.com/users/yash2696/orgs',
+  //   'repos_url': 'https://api.github.com/users/yash2696/repos',
+  //   'events_url': 'https://api.github.com/users/yash2696/events{/privacy}',
+  //   'received_events_url': 'https://api.github.com/users/yash2696/received_events',
+  //   'type': 'User',
+  //   'site_admin': false,
+  //   'name': 'Yash Agarwal',
+  //   'company': 'NIT, Calicut',
+  //   // 'blog': 'https://yashagarwal.me',
+  //   'location': 'Calicut, India',
+  //   'email': null,
+  //   'hireable': null,
+  //   'bio': 'Open Source Enthusiast, Coder',
+  //   'public_repos': 29,
+  //   'public_gists': 2,
+  //   'followers': 18,
+  //   'following': 39,
+  //   'created_at': '2015-08-10T16:07:14Z',
+  //   'updated_at': '2017-05-25T06:09:50Z'
+  // };
 
-  constructor(private _githubService: GithubService, private _searchComponent: SearchComponent, private _route: ActivatedRoute, ) { }
+  username;
+  isLoading: Boolean = false;
+
+  constructor(private _githubService: GithubService, private _searchComponent: SearchComponent, private _route: ActivatedRoute, ) {
+    this.username = this.getUserName()['source']['value']['id'];
+    console.log(this.username);
+   }
 
   ngOnInit() {
-    this._route.params
-      .map(params => {
-        params['id'];
-        this.username = params['id'];
-      })
+    this.getUserName()
       .switchMap(id => this._githubService.getUserDetail(id))
       .subscribe(details => {
-        this.userDetail = details;
-        // console.log(details);
+        this.details = details;
+        console.log(this.details);
         this.isLoading = false;
-        // console.log(this.isLoading);
       },
       null,
       () => {
         this.isLoading = false;
-        // console.log(this.isLoading);
       });
+  }
+
+  getUserName() {
+    return this._route.params
+      .map(params => params['id']);
   }
 }
